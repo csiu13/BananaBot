5e467a30838fdc75fdd35b51de125f3bf257d2ec
Banana <chrisissiu@gmail.com>
2019-11-22 13:47:53 -0500

    Added properties file usage and gitignore

--- BananaBot/src/main/java/com/BottBuddies/BananaBot.java
+++ BananaBot/src/main/java/com/BottBuddies/BananaBot.java
@@ -4,13 +4,23 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 import javax.security.auth.login.LoginException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class BananaBot {
 
     public static void main(String[] args) throws LoginException
-
     {
-        JDA jda = new JDABuilder("NjQyNDM0Nzk3NzIwODI5OTYy.XdQC7Q.I9WumhdqtgQddgLKuTW5EbzNd_c").build();
-        jda.addEventListener(new BananaListener());
+        try {
+            Properties prop = new Properties();
+            prop.load(new FileInputStream("D:\\config.properties"));
+            String token = prop.getProperty("token");
+            JDA jda = new JDABuilder(token).build();
+            jda.addEventListener(new BananaListener());
+        } catch(IOException e) {
+            System.out.println("Error reading in properties file");
+            e.printStackTrace();
+        }
     }
 }
--- BananaBot/src/main/java/com/BottBuddies/BananaListener.java
+++ BananaBot/src/main/java/com/BottBuddies/BananaListener.java
@@ -5,20 +5,31 @@ import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.HashMap;
+
 public class BananaListener extends ListenerAdapter {
+
+    private HashMap<String, Integer> events;
+
+    //Add commands here
+    public BananaListener() {
+        events = new HashMap<String, Integer>();
+        events.put("!ping", 0);
+    }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event)
     {
-        //Ignore events from other bots, including ourself
+        //Ignore events from other bots, including ourselves
         if(event.getAuthor().isBot()) {
             return;
         }
 
         Message message = event.getMessage();
         String content = message.getContentRaw();
+        int index = events.containsKey(content) ? events.get(content) : -1;
 
-        if(content.equals("!ping")) {
+        if(index == 0) {
             MessageChannel channel = event.getChannel();
             channel.sendMessage("Pong").queue();
         }
